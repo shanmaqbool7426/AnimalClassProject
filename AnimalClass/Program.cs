1bce4af4e3d99197879a0209042b46bca3e035fc
shanmaqbool7426 <67848440+shanmaqbool7426@users.noreply.github.com>
2020-08-08 22:32:31 +0500

    Add project files.

---
+++ AnimalClass/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace AnimalClass
+{
+   public abstract class Animal
+   {
+        protected object AAnimal;
+        protected abstract void Move();
+        protected abstract void Eat(string FoodType);
+        public Animal(object anmal)
+        {
+            this.AAnimal = anmal;
+        }
+   }
+    public class Dog : Animal
+    {
+      public Dog(object anmal):base(anmal)
+      {
+            AAnimal = anmal;
+      }
+
+        protected override void Eat(string FoodType)
+        {
+            Console.WriteLine(" Dog foodtype is Meat"+FoodType);
+        }
+
+        protected override void Move()
+        {
+            Console.WriteLine("Dog move fastly");
+        }
+
+    }
+    public class Cat : Animal
+    {
+        public Cat(object animal):base(animal)
+        {
+            AAnimal = animal;
+        }
+
+        protected override void Eat(string FoodType)
+        {
+            Console.WriteLine("cat foodtype is Meat" + FoodType);
+
+
+        }
+
+        protected override void Move()
+        {
+            Console.WriteLine("Cat move fastly");
+
+        }
+    }
+    public class Goat : Animal
+    {
+        public Goat(object animal) : base(animal)
+        {
+            AAnimal = animal;
+        }
+        protected override void Eat(string FoodType)
+        {
+            Console.WriteLine("Goat foodtype is harbivore" + FoodType);
+
+        }
+
+        protected override void Move()
+        {
+            Console.WriteLine("Goat move fastly");
+        }
+    }
+    public class Human : Animal
+    {
+        public Human(object animal) : base(animal)
+        {
+            AAnimal = animal;
+        }
+        protected override void Eat(string FoodType)
+        {
+            Console.WriteLine("human foodtype is both " + FoodType);
+        }
+
+        protected override void Move()
+        {
+            Console.WriteLine("Goat move Slowly");
+        }
+    }
+    public class Lion : Animal
+    {
+        public Lion(object animal) : base(animal)
+        {
+            AAnimal = animal;
+        }
+
+        protected override void Eat(string FoodType)
+        {
+            Console.WriteLine("Lion foodtype is Meat" + FoodType);
+
+        }
+
+        protected override void Move()
+        {
+            Console.WriteLine("Lion move very fastly");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var arraylist = new ArrayList();
+            int count = 1;
+            object d=null;
+            while(count!=5)
+            {
+                Console.WriteLine("enter a number to select the animal... ");
+                int choice=Convert.ToInt32(Console.ReadLine());
+                if(choice==1)
+                {
+                    Console.WriteLine("Add animal Dog... ");
+                     object add = Console.ReadLine();
+                     d = new Dog(add);
+                    arraylist.Add(d);
+                    count++;
+                }
+                if (choice == 2)
+                {
+                    Console.WriteLine("Add animal cat... ");
+                    object add = Console.ReadLine();
+                     d = new Cat(add);
+                    arraylist.Add(d);
+                    count++;
+                }
+                if (choice == 3)
+                {
+                    Console.WriteLine("Add animal Goat... ");
+                    object add = Console.ReadLine();
+                     d = new Goat(add);
+                    arraylist.Add(d);
+                    count++;
+                }
+                if (choice == 4)
+                {
+                    Console.WriteLine("Add  human... ");
+                    object add = Console.ReadLine();
+                     d = new Human(add);
+                    arraylist.Add(d);
+                    count++;
+                }
+                if (choice == 5)
+                {
+                    Console.WriteLine("Add animal Lion... ");
+                    object add = Console.ReadLine();
+                     d = new Lion(add);
+                    arraylist.Add(d);
+                    count++;
+                }
+                if(choice==6)
+                {
+                    Console.WriteLine("animal.....");
+                    foreach (Animal items in arraylist)
+                    {
+                        Console.WriteLine(items);
+                    }
+                }
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
